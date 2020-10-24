cdb3e8ba5b2166ba93b11ebf652652cdb8377af1
Nastya <asekerich99@gmail.com>
2020-10-25 00:57:37 +0300

    feat: add weather functions

--- momentum/script.js
+++ momentum/script.js
@@ -5,7 +5,13 @@ const time = document.querySelector ('.time'),
   focus = document.querySelector ('.focus'),
   dayOfTheWeeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
   monthes = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  playIcon = document.querySelector ('.play__icon');
+  playIcon = document.querySelector ('.play__icon'),
+  weatherIcon =  document.querySelector ('.weather_icon'),
+  temperature = document.querySelector('.temperature'),
+  humidity = document.querySelector('.humidity'),
+  cityInput = document.querySelector('.city'),
+  wind = document.querySelector('.wind');
+
 
 let bgCollection = ['night/04', 'night/18', 'night/09', 'night/07', 'night/08', 'night/19', 'morning/01', 'morning/04', 'morning/18', 'morning/08', 'morning/10', 'morning/12', 'day/02', 'day/04', 'day/05', 'day/20', 'day/09', 'day/12', 'evening/16', 'evening/10', 'evening/18', 'evening/07', 'evening/08', 'evening/19'],
   bgIndex,
@@ -200,8 +206,6 @@ function showRandomImages() {
 
     if(hour >= 6 && hour < 12 ) {
       const imagesOfHours = curentBgCollection.slice(6,12);
-      console.log(imagesOfHours)
-      console.log(curentBgCollection);
 
       imagesOfHours.push(imagesOfHours[0]);
       imagesOfHours.shift();
@@ -217,9 +221,6 @@ function showRandomImages() {
 
       curentBgCollection.splice(12, 6, imagesOfHours);
       bgCollection = curentBgCollection.flat();
-      
-      console.log(imagesOfHours)
-      console.log(curentBgCollection);
 
       localStorage.setItem('backgroundCollection', bgCollection);
     } else if (hour >= 18 && hour <= 23) {
@@ -242,9 +243,82 @@ function showRandomImages() {
       localStorage.setItem('backgroundCollection', bgCollection);
     }
   }
+};
+
+//Get Weather 
+async function getWeather() {
+  let city = localStorage.getItem('cityInput');
+
+  const API_WEATHER = '91e2628fa9db2e79ace3516645c64da8';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=${API_WEATHER}&units=metric`;
+  const res = await fetch(url);
+
+  if (!(res.ok)) {
+    alert('Try to choose city again, please.');
+    cityInput.textContent = '[Enter your city]' ;
+    localStorage.setItem ('cityInput', '[Enter your city]');
+    temperature.textContent = '';
+    humidity.textContent = '';
+    wind.textContent = '';
+    weatherIcon.style.display = 'none';
+
+  } else {
+    const data = await res.json();
+
+    weatherIcon.style.display = 'flex';
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    humidity.textContent = `${data.main.humidity}%`;
+    wind.textContent = `${data.wind.speed.toFixed(0)} m/s`;
+  }
 }
+
+// Get City
+function getCity() {
+  if(localStorage.getItem('cityInput') === null) {
+    cityInput.textContent = '[Enter your city]' ;
+    localStorage.setItem ('cityInput', '[Enter your city]');
+  } else {
+    cityInput.textContent = localStorage.getItem('cityInput');
+  }
+  getWeather();
+};
+
+//Set City 
+function setCity(event) {
+  if (event.type === 'keypress') {
+    //Make sure enter is pressed
+    if (event.which == 13 || event.keyCode == 13) {
+      if (event.target.innerText.trim() === '') {
+        event.preventDefault();
+        cityInput.textContent = localStorage.getItem('cityInput');
+      } else {
+        localStorage.setItem('cityInput', event.target.innerText);
+        cityInput.blur();
+      }
+    }
+  } else {
+    if (event.target.innerText.trim() === '') {
+      cityInput.textContent = localStorage.getItem('cityInput');
+    } else {
+      localStorage.setItem('cityInput', event.target.innerText);
+      cityInput.blur();
+    }
+    getWeather();
+  }
+
+}
+
+cityInput.addEventListener('keypress', setCity);
+cityInput.addEventListener('blur', setCity);
+cityInput.addEventListener('click', (event) => {
+  event.target.innerText = '';
+  cityInput.focus();
+})
 
 //Run
 showTime();
 getName();
 getFocus();
+getCity();
