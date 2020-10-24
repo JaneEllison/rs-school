b4a74d4f0741b0dc8afbbcfe5d1ad84217de8093
Nastya <asekerich99@gmail.com>
2020-10-24 03:04:50 +0300

    feat: add bg functions

--- momentum/script.js
+++ momentum/script.js
@@ -1,20 +1,15 @@
-// но у мен яэто один метод
-// где есть условие - если была перезагрузка - то берем рандомную. и запоминаем позицию
-// если клик по кнопке  - берем след картинку
-
-
-
-
-
 // DOM Elements
 const time = document.querySelector ('.time'),
   greeting = document.querySelector ('.greeting'),
   name = document.querySelector ('.name'),
   focus = document.querySelector ('.focus'),
   dayOfTheWeeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  monthes = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-//Options
-const showAmPm = true;
+  monthes = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  playIcon = document.querySelector ('.play__icon');
+
+let bgCollection = ['night/01', 'night/03', 'night/09', 'night/07', 'night/15', 'night/18', 'morning/01', 'morning/03', 'morning/04', 'morning/07', 'morning/09', 'morning/11', 'day/02', 'day/04', 'day/06', 'day/08', 'day/10', 'day/12', 'evening/05', 'evening/10', 'evening/13', 'evening/15', 'evening/17', 'evening/03'],
+  bgIndex,
+  startHours;
 
 // Show Time
 function showTime() {
@@ -27,9 +22,7 @@ function showTime() {
     dayOfTheWeek = today.getDay();
 
   //Output Time
-  time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} <br> ${monthes[month]} ${day}<span>, </span>${dayOfTheWeeks[dayOfTheWeek]}`;
-
-  setTimeout(showTime, 1000);
+  time.innerHTML = `<span id='hours'>${addZero(hour)}</span><span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} <br> ${monthes[month]} ${day}<span>, </span>${dayOfTheWeeks[dayOfTheWeek]}`;
 };
 
 //Add Zero
@@ -37,32 +30,68 @@ function addZero(n) {
   return (parseInt(n, 10) < 10 ? '0': '') + n;
 };
 
+setInterval(()=> {showTime(); changeBg()}, 1000);
+
+//Change Background
+function changeBg() {
+  let curentHoursElement = document.getElementById('hours');
+  let curentHours = curentHoursElement.textContent;
+  if (startHours != curentHours) {
+    startHours = curentHours;
+    setBackground();
+  }
+};
+
+function saveStartHours(){
+  let today = new Date();
+  return today.getHours();
+}
+
 //Set Background
 
-function setBgGreet() {
+function setBackground() {
+  showRandomImages();
+
   let today = new Date(),
     hour = today.getHours();
-
-  if(hour < 12 && hour >= 6) {
-    //Morning
-    document.body.style.backgroundImage = "url('assets/images/morning/01.jpg";
-    greeting.textContent = 'Good Morning,';
   
-  } else if (hour >= 12 && hour < 18) {
-    //Afternoon
-    document.body.style.backgroundImage = "url('assets/images/day/01.jpg";
-    greeting.textContent = 'Good Day,';
+  bgIndex = hour;
 
+  if(hour >= 6 && hour < 12 ) {
+    const img = document.createElement('img');
+    img.src = `./assets/images/${bgCollection[hour]}.jpg`;
+
+    img.onload = () => {
+      document.body.style.backgroundImage = `url(${img.src})`
+    }
+    greeting.textContent = 'Good Morning,';  
+  } else if (hour >= 12 && hour < 18) {
+    const img = document.createElement('img');
+    img.src = `./assets/images/${bgCollection[hour]}.jpg`;
+
+    img.onload = () => {
+      document.body.style.backgroundImage = `url(${img.src})`
+    }
+    greeting.textContent = 'Good Day,';  
   } else if (hour >= 18 && hour <= 23) {
-    //Evening
-    document.body.style.backgroundImage = "url('assets/images/evening/01.jpg";
-    greeting.textContent = 'Good Evening,';
-    
+    const img = document.createElement('img');
+    img.src = `./assets/images/${bgCollection[hour]}.jpg`;
+
+    img.onload = () => {
+      document.body.style.backgroundImage = `url(${img.src})`
+    }
+    greeting.textContent = 'Good Evening,';  
   } else {
-    document.body.style.backgroundImage = "url('assets/images/night/01.jpg";
-    greeting.textContent = 'Good Nigth,';
-    document.body.style.color = 'white';
+    const img = document.createElement('img');
+    img.src = `./assets/images/${bgCollection[hour]}.jpg`;
+
+    img.onload = () => {
+      document.body.style.backgroundImage = `url(${img.src})`
+    }
+    greeting.textContent = 'Good Night,';  
   }
+
+
 };
 
 // Get Name
@@ -74,8 +103,6 @@ function getName() {
     name.textContent = localStorage.getItem('userName');
   }
 };
-
-getName();
 
 //Set Name 
 function setName(event) {
@@ -139,13 +166,6 @@ function getFocus() {
   }
 }
 
-// name.addEventListener('keypress', setName);
-// name.addEventListener('blur', setName);
-// name.addEventListener('click', (event) => {
-//   event.target.innerText = '';
-//   name.focus();
-// });
-
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
 focus.addEventListener('click', (event) => {
@@ -153,10 +173,73 @@ focus.addEventListener('click', (event) => {
   focus.focus();
 });
 
+//Show all Bg-s
+playIcon.addEventListener ('click', () =>{
+  bgIndex += 1;
 
+  if (bgIndex === 24) {
+    bgIndex = 0;
+  }
+
+  const img = document.createElement('img');
+  img.src = `./assets/images/${bgCollection[bgIndex]}.jpg`;
+  img.onload = () => {
+    document.body.style.backgroundImage = `url(${img.src})`;
+  };
+})
+
+//show Random bg
+function showRandomImages() {
+  let curentBgCollection = localStorage.getItem('backgroundCollection');
+
+  if(!curentBgCollection) {
+    localStorage.setItem('backgroundCollection', bgCollection);
+  } else {
+    curentBgCollection = curentBgCollection.split(',');
+    const hour = new Date ().getHours();
+
+    if(hour >= 6 && hour < 12 ) {
+      const imagesOfHours = curentBgCollection.slice(6,13);
+
+      imagesOfHours.push(imagesOfHours[0]);
+      imagesOfHours.shift();
+      curentBgCollection.splice(6, 7, imagesOfHours);
+
+      bgCollection = curentBgCollection.flat();
+      localStorage.setItem('backgroundCollection', bgCollection);
+    } else if (hour >= 12 && hour < 18) {
+      const imagesOfHours = curentBgCollection.slice(13, 18);
+      
+      imagesOfHours.push(imagesOfHours[0]);
+      imagesOfHours.shift();
+
+      curentBgCollection.splice(13, 5, imagesOfHours);
+      bgCollection = curentBgCollection.flat();
+      localStorage.setItem('backgroundCollection', bgCollection);
+    } else if (hour >= 18 && hour <= 23) {
+      const imagesOfHours = curentBgCollection.slice(18);
+
+      imagesOfHours.push(imagesOfHours[0]);
+      imagesOfHours.shift();
+
+      curentBgCollection.splice(18, 6, imagesOfHours);
+      bgCollection = curentBgCollection.flat();
+      localStorage.setItem('backgroundCollection', bgCollection);
+    } else {
+      const imagesOfHours = curentBgCollection.slice(0, 6);
+
+      imagesOfHours.push(imagesOfHours[0]);
+      imagesOfHours.shift();
+
+      curentBgCollection.splice(0, 6, imagesOfHours);
+      bgCollection = curentBgCollection.flat();
+      localStorage.setItem('backgroundCollection', bgCollection);
+    }
+  }
+}
 
 //Run
 showTime();
-setBgGreet();
-// getName();
+setBackground();
+getName();
 getFocus();
