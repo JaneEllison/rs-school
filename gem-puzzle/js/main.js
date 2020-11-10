e5e84486717d83e4b304d691d2f85978d2098aa1
Nastya <asekerich99@gmail.com>
2020-11-11 00:15:22 +0300

    feat:add cels

--- gem-puzzle/js/main.js
+++ gem-puzzle/js/main.js
@@ -1,1 +1,24 @@
-console.log ('hello');
+'use strict';
+
+const field = document.querySelector('.field');
+const cellSize = 100;
+
+const empty = {
+  top: 300,
+  left: 300
+};
+
+for (let i = 0; i < 15; i++) {
+  const cell = document.createElement ('div'); 
+  cell.className = 'cell';
+  cell.innerHTML = i + 1;
+
+  const left = i % 4;
+  const top = (i - left) / 4;
+
+  cell.style.left = `${left * cellSize}px`;
+  cell.style.top = `${top * cellSize}px`;
+
+  field.append(cell);
+  
+};
