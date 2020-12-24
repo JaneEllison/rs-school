ebd6511f0a4d6eb638f96ce2d1ab050b94cbf1c8
Nastya <asekerich99@gmail.com>
2020-12-24 12:17:24 +0300

    feat: add covid-dashboard

---
+++ covid-dashboard/hodgef-simple-keyboard-2.32.0-277-g8264ae1/hodgef-simple-keyboard-8264ae1/src/demo/index.js
@@ -0,0 +1,21 @@
+import "./css/index.css";
+
+/**
+ * Demos
+ */
+import BasicDemo from "./BasicDemo";
+//import RTLDemo from "./RTLDemo";
+//import ButtonThemeDemo from "./ButtonThemeDemo";
+//import DOMElementDemo from "./DOMElementDemo";
+//import FullKeyboardDemo from "./FullKeyboardDemo";
+//import MultipleKeyboardsDemo from "./MultipleKeyboardsDestroyDemo";
+
+/**
+ * Selected demo
+ */
+const SELECTED_DEMO = BasicDemo;
+
+/**
+ * Bootstrap
+ */
+new SELECTED_DEMO();
